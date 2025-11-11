d8759eaf5c6bd553b9fe2c7ff87aa6e11a823f88
Adam Read <aread03@qub.ac.uk>
2025-11-11 18:03:00 +0000

    Skeletal code made

--- countryinfo.cpp
+++ countryinfo.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+#include "countryinfo.h"
 using namespace std;
 
+//split the input into arrays
 string countryinfo(const string& ip){
     unordered_map<int,string> country_map = {
         {100,"US"},{101,"UK"},{102,"China"}
     };
+    //to do: exclude ipv6 addresses
     if (ip.size() < 3) return "Unknown";
     int prefix = stoi(ip.substr(0,3));
     if (country_map.count(prefix)){

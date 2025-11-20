a301a06de2f2655e953d5274e99a6da4ec043eed
Adam Read <aread03@qub.ac.uk>
2025-11-20 11:18:58 +0000

    Java Classifier test

---
+++ test/Classifier_test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class Classifier_test{
+    @Test
+    void classifyIPs(){
+        String[] testInput = {};
+        String[] testExpected = {};
+        String[] testResult = {};
+        assertArrayEquals(testExpected,testResult);
+    }
+}

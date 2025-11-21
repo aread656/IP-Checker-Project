42de975eefc628a77bad32562b3936a055ca8d65
Adam Read <aread03@qub.ac.uk>
2025-11-21 15:34:15 +0000

    Handling for "::" and subsequent CI tests

--- test/test-validips.py
+++ test/test-validips.py
@@ -2,17 +2,14 @@ import sys
 sys.path.insert(0,".");
 from totalvalidips import totalValidIPs
 
-"""it('Bad IP Test', function(done){
-    var t = ["127.0.0.1",
-        "100.200.300.400",
-        "21:aa:3b:22:77:9a",
-        "101.201.301.401"]
-    var a = ["good","bad","good","bad"]
-    expect(badipcount.badIPs(t)).to.deep.equal(a);
-    done()
-})"""
-
 def main():
+    print(test1())
+    print(test2())
+    
+if __name__ == "__main__":
+    main()
+
+def test1():
     test_success = True;
     t = ["127.0.0.1",
         "100.200.300.400",
@@ -28,6 +25,79 @@ def main():
         sys.exit(0)
     else:
         sys.exit(1)
+
+def test2():
+    test_success = True;
+    t = ["::1","1.1.1.1"]
+    a = 4
+    r = totalValidIPs(t)
+    if (r!=a):
+        test_success = False
     
-if __name__ == "__main__":
-    main()
+    if (test_success):
+        sys.exit(0)
+    else:
+        sys.exit(1)
+
+"""valid_ipv4s = [
+    "0.0.0.0",              # lowest IPv4
+    "255.255.255.255",      # highest IPv4
+    "1.2.3.4",
+    "192.168.1.1",
+    "127.0.0.1",            # loopback
+    "8.8.8.8",
+    "0.1.2.3",              # leading zero in a segment
+    "1.2.3.004",            # leading zeros (textually weird but numeric)
+    "192.168.001.010"
+]
+
+invalid_ipv4 = [
+    "256.100.100.100",      # out of range
+    "192.168.1",            # too few groups
+    "192.168.1.1.1",        # too many groups
+    "192.168..1",           # empty group
+    "...",                  # all empty
+    "1.2.3.-4",             # negative number
+    "1.2.3.4a",             # invalid character
+    "abc.def.ghi.jkl",      # no digits
+    "1.2.3.",               # trailing dot
+    ".1.2.3"                # leading dot
+]
+
+valid_ipv6s = [
+    "::",                                   # zero-compressed
+    "::1",                                  # loopback
+    "1::",                                  # trailing ::
+    "::1:2:3:4:5:6:7",                      # leading ::
+    "1:2:3:4:5:6:7::",                      # trailing ::
+    "0:0:0:0:0:0:0:1",                      # loopback full
+    "2001:db8::",                           # documentation prefix
+    "0aaa::1",                              # your test case
+    "1a:2a:3a:4a:5a:6a::",                  # missing last groups
+    "1a:2a:3a:4a:5a:6a:7a:8a",              # full 8 groups
+    "abcd:ef01:2345:6789:abcd:ef01:2345:6789",  # full/max-length normal
+    "0000:0000:0000:0000:0000:0000:0000:0000",  # explicit zero
+    "FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF",  # max
+    "::ffff:192.168.1.1"                    # IPv4-mapped IPv6
+]
+
+invalid_ipv6 = [
+    "1:2:3:4:5:6:7:8:9",            # too many groups
+    "1:2:3:4:5:6:7",                # too few groups w/o ::
+    "1::2::3",                      # multiple ::
+    ":::1",                         # triple colon
+    "1:2:3:4:5::6:7:8:9",           # too many after expansion
+    "1:2:3:4:5:6:G:1",              # invalid hex digit
+    "::00000",                      # >4 hex digits in a group
+    "1:2:3:4:",                     # trailing colon w/o ::
+    ":1:2:3:4:5:6:7",               # leading colon w/o ::
+    "1:2:3:::",                     # malformed
+    "",                             # empty string
+    ":::::::",                      # nonsense
+]
+
+print(totalValidIPs(valid_ipv4s))
+print(totalValidIPs(invalid_ipv4))
+print(totalValidIPs(valid_ipv6s))
+print(totalValidIPs(invalid_ipv6))
+"""
--- totalvalidips.py
+++ totalvalidips.py
@@ -1,21 +1,57 @@
-#for IPv4, check if there are 4 groups of values separated by dots; 
-#and for IPv6, check if there are up to 8 groups (you can have 2/3/4/5/6/7/8 groups) of values separated by colons; 
-#no need to check if the values are decimal/hexadecimal, 
-#and no need to check number of digits or characters in each group.
-
 def totalValidIPs(ips):
     num_valid = 0
     for ip in ips:
         if "." in ip:
-            #"." means ipv4
-            ip_parts = ip.split(".")
-            if len(ip_parts) == 4:
-                num_valid = num_valid + 1
+            if (valid_ipv4(ip)):
+                num_valid += 1
         elif ":" in ip:
-            #":" means ipv6
-            ip_parts = ip.split(":")
-            if 2 < len(ip_parts) <= 8:
-                num_valid = num_valid + 1
+           if (valid_ipv6(ip)):
+               num_valid += 1;
         else:
             continue
     return num_valid
+
+def valid_ipv4(ip):
+    ip_parts = ip.split(".")
+    if len(ip_parts) != 4:
+        return False
+    for part in ip_parts:
+        if(not part.isdigit() or not (0<=int(part)<=255)):
+            return False
+    return True
+
+def valid_ipv6(ip):
+    if ip.count("::") >= 2:
+        return False
+    
+    if "::" in ip:
+        LHS, RHS = ip.split("::")
+        if LHS:
+            LHS_parts = LHS.split(":")
+        else:
+            LHS_parts = []
+        if RHS:
+            RHS_parts = RHS.split(":")
+        else:
+            RHS_parts = []
+        empty_parts = 8 - (len(LHS_parts) + len(RHS_parts))
+        ip_parts = LHS_parts + (["0000"]*empty_parts) + RHS_parts
+    else:
+        ip_parts = ip.split(":")
+
+    #should now be eight parts
+    if len(ip_parts) != 8:
+        return False
+    
+    for part in ip_parts:
+        if len(part) > 4 or len(part) == 0:
+            return False
+        for char in part.lower():
+            if char.lower() not in "0123456789abcdef":
+                return False
+    return True
+
+#2001:0db8:85a3:0000:0000:8a2e:0370:7334
+#parts = [2001, 0db8, 85a3, 0000, 0000, 8a2e, 0370, 7334]
+#shortened = 2001:0db8:85a3::8a2e:0370:7334
+

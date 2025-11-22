a36a2bc41667d57d053ac253332b0f701c473ccb
Adam Read <aread03@qub.ac.uk>
2025-11-22 13:20:01 +0000

    Tests correction

--- test/test_countryinfo.cpp
+++ test/test_countryinfo.cpp
@@ -9,7 +9,7 @@ int main() {
     assert(countryinfo("102.202.302.402")=="China");
     assert(countryinfo("103.203.303.403")=="Unknown");
     assert(countryinfo("a")=="Unknown");   
-    assert(countryinfo("::1") == "Unknown");
+    assert(countryinfo("::1") == "IPV6");
     assert(countryinfo("a:.43") == "Unknown" || countryinfo("a:.43") == "IPV6");
     return 0;
 }

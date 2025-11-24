ae220dde035d62a3f2fccd69a06cfb9b16526145
Adam Read <aread03@qub.ac.uk>
2025-11-24 11:08:30 +0000

    Errors in number of expected passes

--- src/test.php
+++ src/test.php
@@ -5,14 +5,14 @@ $fail = 0;
 function test($result, $expected): void{
     global $fail;
     if ($result !== $expected){
-        echo "Fail: got $result, expected %expected\n";
+        echo "Fail: got $result, expected $expected\n";
         $fail = 1;
     }else{
         echo "Pass: %result == $expected\n";
     }
 }
 
-test(getTotalIPs("1.1.1.1,2.2.2.2,3.3.3.3"),2);
+test(getTotalIPs("1.1.1.1,2.2.2.2,3.3.3.3"),3);
 test(getTotalIPs(""),1);
 test(getTotalIPs("1:2:3:4:"),1);
 test(getTotalIPs("a.b.c.d"),1);

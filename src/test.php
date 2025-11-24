dc87c2960bf5ad24977ca92c29ca19dfb7bc9e31
Adam Read <aread03@qub.ac.uk>
2025-11-24 11:06:47 +0000

    More error fixing

--- src/test.php
+++ src/test.php
@@ -12,10 +12,10 @@ function test($result, $expected): void{
     }
 }
 
-test(result: getTotalIPs(items: "1.1.1.1,2.2.2.2,3.3.3.3"),expected: 2);
-test(result: getTotalIPs(items: ""),expected: 1);
-test(result: getTotalIPs(items: "1:2:3:4:"),expected: 1);
-test(result: getTotalIPs(items: "a.b.c.d"),expected: 1);
-test(result: getTotalIPs(items: "127.0.0.1"),expected: 1);
+test(getTotalIPs("1.1.1.1,2.2.2.2,3.3.3.3"),2);
+test(getTotalIPs(""),1);
+test(getTotalIPs("1:2:3:4:"),1);
+test(getTotalIPs("a.b.c.d"),1);
+test(getTotalIPs("127.0.0.1"),1);
 
 exit($fail);

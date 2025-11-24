9d94bac45be7374636783be08985131fc3465c4c
Adam Read <aread03@qub.ac.uk>
2025-11-24 11:03:18 +0000

    Fixing php syntax errors

--- src/test.php
+++ src/test.php
@@ -1,8 +1,9 @@
+<?php
 require 'functions.inc.php';
 $fail = 0;
 
-function test($result, $expected){
-    global %fail
+function test($result, $expected): void{
+    global $fail;
     if ($result !== $expected){
         echo "Fail: got $result, expected %expected\n";
         $fail = 1;
@@ -11,10 +12,10 @@ function test($result, $expected){
     }
 }
 
-check(getTotalIPs("1.1.1.1,2.2.2.2,3.3.3.3"),2);
-check(getTotalIPs(""),1)
-check(getTotalIPs("1:2:3:4:"),1)
-check(getTotalIPs("a.b.c.d"),1)
-check(getTotalIPs("127.0.0.1"),1)
+test(result: getTotalIPs(items: "1.1.1.1,2.2.2.2,3.3.3.3"),expected: 2);
+test(result: getTotalIPs(items: ""),expected: 1);
+test(result: getTotalIPs(items: "1:2:3:4:"),expected: 1);
+test(result: getTotalIPs(items: "a.b.c.d"),expected: 1);
+test(result: getTotalIPs(items: "127.0.0.1"),expected: 1);
 
 exit($fail);

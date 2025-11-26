ae3bee73bc921d81c9662d2deac4201a982dc959
Adam Read <aread03@qub.ac.uk>
2025-11-26 21:12:23 +0000

    Basic validation included

--- server.js
+++ server.js
@@ -9,15 +9,25 @@ var badips = require('./badips');
 
 const app = express();
 app.get('/', (req,res) => {
-
+    const validIPChars = "0123456789abcdef.:, "
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*')
-    var text = req.query.items;
+    var text = req.query.items.trim();
     if (!text){
         res.end(JSON.stringify({
             error: true, input: [], answer: [],
             message: "No inputs were given"
         }))
+        return;
+    }
+    for (let c of text){
+        if(!validIPChars.includes(c)){
+            res.end(JSON.stringify({
+                error:true,input:text, answer:[],
+                message:"invalid characters inputted"
+            }))
+            return;
+        }
     }
     var arrayIPs = text.split(',');
     arrayIPs = arrayIPs.map(IP=>IP.trim())
